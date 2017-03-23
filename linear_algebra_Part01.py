de9399e6a80987fb1c665b26663101e0be824848
wishheart303 <wishheart303@yahoo.com.tw>
2017-03-23 16:29:20 +0800

    2017/03/23 Data Science from Scratch_Unit04

--- linear_algebra_Part01.py
+++ linear_algebra_Part01.py
@@ -8,7 +8,7 @@ from functools import partial
 
 # 
 # functions for working with vectors
-#
+
 
 def vector_add(v, w):
     """adds two vectors componentwise"""
@@ -18,11 +18,21 @@ def vector_subtract(v, w):
     """subtracts two vectors componentwise"""
     return [v_i - w_i for v_i, w_i in zip(v,w)]
 
+a1 = [1, 2]
+a2 = [3, 4]
+print("vector_add(a1) = " + str(vector_add(a1, a2)))
+print("vector_subtract(a2) = " + str(vector_subtract(a1, a2)))
+
 def vector_sum(vectors):
     return reduce(vector_add, vectors)
 
+a3 = [[1, 2], [3, 4], [5, 6]]
+print("vector_sum(a3) = " + str(vector_sum(a3)))
+
 def scalar_multiply(c, v):
     return [c * v_i for v_i in v]
+
+print("scalar_multiply(10, a1) = " + str(scalar_multiply(10, a1)))
 
 # this isn't right if you don't from __future__ import division
 def vector_mean(vectors):
@@ -31,9 +41,13 @@ def vector_mean(vectors):
     n = len(vectors)
     return scalar_multiply(1/n, vector_sum(vectors))
 
+print("vector_mean(a3) = " + str(vector_mean(a3)))
+
 def dot(v, w):
     """v_1 * w_1 + ... + v_n * w_n"""
     return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+print("dot(a1, a2) = " + str(dot(a1, a2)))
 
 def sum_of_squares(v):
     """v_1 * v_1 + ... + v_n * v_n"""
@@ -41,6 +55,8 @@ def sum_of_squares(v):
 
 def magnitude(v):
     return math.sqrt(sum_of_squares(v))
+
+print("magnitude(a1) = " + str(magnitude(a1)))
 
 def squared_distance(v, w):
     return sum_of_squares(vector_subtract(v, w))
