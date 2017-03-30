22f0606097726713601106ddde3eb6843c8d3011
Ling <wishheart303@yahoo.com.tw>
2017-03-30 16:15:48 +0800

    2017/03/30 Data Science from Scratch_Unit04

---
+++ linear_algebra_Part02.py
@@ -0,0 +1,112 @@
+# -*- coding: iso-8859-15 -*-
+
+from __future__ import division # want 3 / 2 == 1.5
+import re, math, random # regexes, math functions, random numbers
+import matplotlib.pyplot as plt # pyplot
+from collections import defaultdict, Counter
+from functools import partial
+
+#
+# functions for working with matrices
+#
+
+A = [[1, 2, 3], # A has 2 rows and 3 columns
+      [4, 5, 6]]
+B = [[1, 2], # B has 3 rows and 2 columns
+     [3, 4],
+     [5, 6]]
+
+def dot(v, w):
+    """v_1 * w_1 + ... + v_n * w_n"""
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+def scalar_multiply(c, v):
+    return [c * v_i for v_i in v]
+
+def shape(A):
+    num_rows = len(A)
+    num_cols = len(A[0]) if A else 0
+    return num_rows, num_cols
+
+print(shape(A))
+
+def get_row(A, i):
+    return A[i]
+
+print(get_row(A, 0))
+
+def get_column(A, j):
+    return [A_i[j] for A_i in A]
+
+print(get_column(A, 1))
+
+def make_matrix(num_rows, num_cols, entry_fn):
+    """returns a num_rows x num_cols matrix 
+    whose (i,j)-th entry is entry_fn(i, j)"""
+    return [[entry_fn(i, j) for j in range(num_cols)]
+            for i in range(num_rows)]
+
+def myfunction(i, j):
+    return 1
+
+print(make_matrix(3, 5, myfunction))
+
+def is_diagonal(i, j):
+    """1's on the 'diagonal', 0's everywhere else"""
+    return 1 if i == j else 0
+
+identity_matrix = make_matrix(5, 5, is_diagonal)
+
+#          user 0  1  2  3  4  5  6  7  8  9
+#
+friendships = [[0, 1, 1, 0, 0, 0, 0, 0, 0, 0], # user 0
+               [1, 0, 1, 1, 0, 0, 0, 0, 0, 0], # user 1
+               [1, 1, 0, 1, 0, 0, 0, 0, 0, 0], # user 2
+               [0, 1, 1, 0, 1, 0, 0, 0, 0, 0], # user 3
+               [0, 0, 0, 1, 0, 1, 0, 0, 0, 0], # user 4
+               [0, 0, 0, 0, 1, 0, 1, 1, 0, 0], # user 5
+               [0, 0, 0, 0, 0, 1, 0, 0, 1, 0], # user 6
+               [0, 0, 0, 0, 0, 1, 0, 0, 1, 0], # user 7
+               [0, 0, 0, 0, 0, 0, 1, 1, 0, 1], # user 8
+               [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]] # user 9
+
+friends_of_five = [i # only need
+                    for i, is_friend in enumerate(friendships[5]) # to look at
+                         if is_friend] # one row
+print(friends_of_five)
+#####
+# DELETE DOWN
+#
+
+
+def matrix_add(A, B):
+    if shape(A) != shape(B):
+        raise ArithmeticError("cannot add matrices with different shapes")
+        
+    num_rows, num_cols = shape(A)
+    def entry_fn(i, j): return A[i][j] + B[i][j]
+        
+    return make_matrix(num_rows, num_cols, entry_fn)
+
+
+def make_graph_dot_product_as_vector_projection(plt):
+
+    v = [2, 1]
+    w = [math.sqrt(.25), math.sqrt(.75)]
+    c = dot(v, w)
+    vonw = scalar_multiply(c, w)
+    o = [0,0]
+
+    plt.arrow(0, 0, v[0], v[1], 
+              width=0.002, head_width=.1, length_includes_head=True)
+    plt.annotate("v", v, xytext=[v[0] + 0.1, v[1]])
+    plt.arrow(0 ,0, w[0], w[1], 
+              width=0.002, head_width=.1, length_includes_head=True)
+    plt.annotate("w", w, xytext=[w[0] - 0.1, w[1]])
+    plt.arrow(0, 0, vonw[0], vonw[1], length_includes_head=True)
+    plt.annotate(u"(v•w)w", vonw, xytext=[vonw[0] - 0.1, vonw[1] + 0.1])
+    plt.arrow(v[0], v[1], vonw[0] - v[0], vonw[1] - v[1], 
+              linestyle='dotted', length_includes_head=True)
+    plt.scatter(*zip(v,w,o),marker='.')
+    plt.axis('equal')
+    plt.show()
